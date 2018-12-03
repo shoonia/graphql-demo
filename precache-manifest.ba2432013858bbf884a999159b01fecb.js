e68e6a3a30292502c7cd9d4fdf8407852c794b02
shoonia <zaytsev126@gmail.com>
2018-12-03 11:28:30 +0200

    Updates

---
+++ precache-manifest.ba2432013858bbf884a999159b01fecb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7adf6f395fdccf2f1550",
+    "url": "/graphql-demo/static/js/runtime~main.7adf6f39.js"
+  },
+  {
+    "revision": "19cc2f439b10e4498c92",
+    "url": "/graphql-demo/static/js/main.19cc2f43.chunk.js"
+  },
+  {
+    "revision": "f21ce20f68667a093429",
+    "url": "/graphql-demo/static/js/1.f21ce20f.chunk.js"
+  },
+  {
+    "revision": "19cc2f439b10e4498c92",
+    "url": "/graphql-demo/static/css/main.ffe74ff5.chunk.css"
+  },
+  {
+    "revision": "7b8ce675a2b4d7b39fca80615e472916",
+    "url": "/graphql-demo/index.html"
+  }
+];
